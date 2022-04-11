2196091bb25c8ed62cf6454898b2a11f2d3cd9b5
sudongyu <976499226@qq.com>
2022-04-11 23:21:11 +0800

    chore: add firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBp_JtpFYlJcVFt0Aj_UmdE5LgVhEpKWOA",
+  authDomain: "slack-clone-127d2.firebaseapp.com",
+  projectId: "slack-clone-127d2",
+  storageBucket: "slack-clone-127d2.appspot.com",
+  messagingSenderId: "355772518213",
+  appId: "1:355772518213:web:4a540634d04b30b2e6382c"
+};
+ 
