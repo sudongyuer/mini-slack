a506f1ae303e9da56244999d7cd846bb83167047
sudongyu <976499226@qq.com>
2022-04-12 00:55:24 +0800

    feat: add Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import { AccountCircleSharp } from '@mui/icons-material';
 function Header() {
   return (
-    <div>Header</div>
+    <HeaderContainer>
+      {/* Header Left */}
+      <HeaderLeft>
+        <HeaderAvatar />
+      </HeaderLeft>
+      {/* Header Search */}
+      {/* Header Right */}
+      <Hello/>
+    </HeaderContainer>
   )
 }
+
+const HeaderContainer = styled.div``
+const HeaderLeft = styled.div`
+background-color: red;
+`
+const HeaderAvatar = styled(AccountCircleSharp)`
+  color:green;
+`
+const Hello1 =function({className}:any){
+  return <div className={className}>123</div>
+}
+
+const Hello =styled(Hello1)`
+background-color: pink;
+`
 
 export default Header
