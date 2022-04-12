f3c7ede75a88cb512ecce2ff8d0bcf80877ffde0
sudongyu <976499226@qq.com>
2022-04-12 22:35:59 +0800

    chore: update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,24 +10,17 @@ function Header() {
       </HeaderLeft>
       {/* Header Search */}
       {/* Header Right */}
-      <Hello/>
     </HeaderContainer>
   )
 }
 
 const HeaderContainer = styled.div``
-const HeaderLeft = styled.div`
-background-color: red;
-`
+const HeaderLeft = styled.div` `
 const HeaderAvatar = styled(AccountCircleSharp)`
   color:green;
 `
 const Hello1 =function({className}:any){
   return <div className={className}>123</div>
 }
-
-const Hello =styled(Hello1)`
-background-color: pink;
-`
 
 export default Header
