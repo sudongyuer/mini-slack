502370f6a4f795f362c1057286628901c402814b
sudongyu <976499226@qq.com>
2022-04-12 22:54:38 +0800

    chore: achivee header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
-import { AccountCircleSharp } from '@mui/icons-material';
+import { AccountCircleSharp ,AccessTime} from '@mui/icons-material';
 function Header() {
   return (
     <HeaderContainer>
       {/* Header Left */}
       <HeaderLeft>
         <HeaderAvatar />
+        <AccessTime/>
       </HeaderLeft>
       {/* Header Search */}
       {/* Header Right */}
@@ -14,13 +15,31 @@ function Header() {
   )
 }
 
-const HeaderContainer = styled.div``
-const HeaderLeft = styled.div` `
-const HeaderAvatar = styled(AccountCircleSharp)`
-  color:green;
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color:white
 `
-const Hello1 =function({className}:any){
-  return <div className={className}>123</div>
-}
+const HeaderLeft = styled.div`
+  flex:0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 30px;
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`
+const HeaderAvatar = styled(AccountCircleSharp)`
+  cursor: pointer;
+  :hover{
+    opacity: 0.8;
+  }
+`
 
 export default Header
