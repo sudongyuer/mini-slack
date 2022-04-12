9940c7bbe0c76a2a2b8437f03072c7ee211e0492
sudongyu <976499226@qq.com>
2022-04-12 23:16:24 +0800

    chore: update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
-import { AccountCircleSharp ,AccessTime} from '@mui/icons-material';
+import { AccountCircleSharp, AccessTime, Search , HelpOutline } from '@mui/icons-material';
 function Header() {
   return (
     <HeaderContainer>
       {/* Header Left */}
       <HeaderLeft>
         <HeaderAvatar />
-        <AccessTime/>
+        <AccessTime />
       </HeaderLeft>
       {/* Header Search */}
+      <HeaderSearch>
+        <Search />
+        <input placeholder="Search" type="text" />
+      </HeaderSearch>
       {/* Header Right */}
+      <HeaderRight>
+        <HelpOutline />
+      </HeaderRight>
     </HeaderContainer>
   )
 }
@@ -20,7 +27,7 @@ const HeaderContainer = styled.div`
   position: fixed;
   width: 100%;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   padding: 10px 0;
   background-color: var(--slack-color);
   color:white
@@ -40,6 +47,28 @@ const HeaderAvatar = styled(AccountCircleSharp)`
   :hover{
     opacity: 0.8;
   }
+`
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: none;
+    color: white;
+  }
+`
+const HeaderRight = styled.div`
+flex: 0.3;
 `
 
 export default Header
