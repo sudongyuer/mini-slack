460332f38e25bfb6f1256af2992a3757d9f9079a
sudongyu <976499226@qq.com>
2022-04-13 00:24:03 +0800

    chore: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,28 @@ import { useState } from 'react'
 import './App.css'
 import { Routes, Route, Link } from "react-router-dom";
 import Header from './components/Header';
+import styled from 'styled-components';
+import Sidebar from './components/Siderbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/"
-          element={
-            <Header/>
-          }
-        >
-        </Route>
-      </Routes>
+      <Header />
+      <AppBody>
+        <Sidebar />
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </AppBody>
     </div>
   )
 }
 
 export default App
+
+const AppBody = styled.div`
+display: flex;
+height: 100vh;
+`
