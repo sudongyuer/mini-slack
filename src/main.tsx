d9b3a49107782967d533f453a3ce6a9f29b15a48
sudongyu <976499226@qq.com>
2022-04-16 21:47:51 +0800

    chore: update Sidebar.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux'
+import store from './app/store';
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
