3e627952d16063000175420e7e80eff41c051c61
sudongyu <976499226@qq.com>
2022-04-18 21:08:58 +0800

    chore: update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,18 @@
-import {initializeApp} from 'firebase/app'
-import {getFirestore} from 'firebase/firestore'
-import {getAuth ,GoogleAuthProvider} from 'firebase/auth'
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { GoogleAuthProvider, getAuth } from 'firebase/auth'
 export const firebaseConfig = {
-  apiKey: "AIzaSyBp_JtpFYlJcVFt0Aj_UmdE5LgVhEpKWOA",
-  authDomain: "slack-clone-127d2.firebaseapp.com",
-  projectId: "slack-clone-127d2",
-  storageBucket: "slack-clone-127d2.appspot.com",
-  messagingSenderId: "355772518213",
-  appId: "1:355772518213:web:4a540634d04b30b2e6382c"
-};
- 
+  apiKey: 'AIzaSyBp_JtpFYlJcVFt0Aj_UmdE5LgVhEpKWOA',
+  authDomain: 'slack-clone-127d2.firebaseapp.com',
+  projectId: 'slack-clone-127d2',
+  storageBucket: 'slack-clone-127d2.appspot.com',
+  messagingSenderId: '355772518213',
+  appId: '1:355772518213:web:4a540634d04b30b2e6382c',
+}
+
 const firebaseApp = initializeApp(firebaseConfig)
-const db=getFirestore(firebaseApp)
+const db = getFirestore(firebaseApp)
 const auth = getAuth(firebaseApp)
 const provider = new GoogleAuthProvider()
 
-export {auth , provider ,db}
+export { auth, provider, db }
