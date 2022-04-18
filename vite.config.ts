8d24c88b25167ff66681f420ffa28b362c5bda6b
sudongyu <976499226@qq.com>
2022-04-18 21:08:36 +0800

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
+import * as path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve:{
-    alias:{
+  resolve: {
+    alias: {
       '@': path.resolve(__dirname, './src'),
-      "@/*":path.resolve(__dirname, './src/*'),
-    }
-  }
+      '@/*': path.resolve(__dirname, './src/*'),
+    },
+  },
 })
