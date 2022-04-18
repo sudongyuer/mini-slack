59fda781019209884d06fe0f39f5419175d7a025
sudongyu <976499226@qq.com>
2022-04-18 08:12:52 +0800

    chore: format

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './app/store';
+import App from './App'
+import store from './app/store'
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -13,5 +14,5 @@ ReactDOM.render(
     </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
