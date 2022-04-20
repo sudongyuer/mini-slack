917099f8e015d84283531733e9dda033ad421bde
sudongyu <976499226@qq.com>
2022-04-21 01:07:46 +0800

    chore: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import { useRef, useState } from 'react'
 import './App.css'
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import styled from 'styled-components'
+import { useAuthState } from 'react-firebase-hooks/auth'
 import Header from './components/Header'
 import Sidebar from './components/Siderbar'
 import Chat from './components/Chat'
-import {useAuthState} from 'react-firebase-hooks/auth'
-import {auth } from './firebase'
+import { auth } from './firebase'
 import Login from './components/Login'
+
 function App() {
-  const [user,loading] = useAuthState(auth)
+  const [user, loading] = useAuthState(auth)
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+            alt=""
+          />
+        </AppLoadingContents>
+      </AppLoading>
+    )
+  }
   return (
     <div className="App">
       {
-        !user?(
+        !user
+          ? (
           <Login/>
-        ):(
+            )
+          : (
           <>
           <Header/>
           <AppBody>
@@ -25,9 +39,9 @@ function App() {
             </Routes>
           </AppBody>
           </>
-        )
+            )
       }
-  
+
     </div>
   )
 }
@@ -37,4 +51,26 @@ export default App
 const AppBody = styled.div`
 display: flex;
 height: 100vh;
+`
+
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
 `
